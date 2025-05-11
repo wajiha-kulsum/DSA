6a1a220c05ed8ae904557b858c6f755319f927e5
Wajiha Kulsum <140605975+wajiha-kulsum@users.noreply.github.com>
2025-05-11 18:25:13 +0530

    Create Valid_Anagram.cpp

---
+++ Valid_Anagram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        unordered_map<char, int> countS;
+        unordered_map<char, int> countT;
+        
+        return countS == countT;
+    }
+};
