0edc8e4014eddcf496fbd14273c211cd6b074ef2
Wajiha Kulsum <140605975+wajiha-kulsum@users.noreply.github.com>
2025-05-13 12:01:06 +0530

    question updated

--- Group_Anagrams.cpp
+++ Group_Anagrams.cpp
@@ -1,3 +1,14 @@
+// Question : Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+
+// An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+// Example 1:
+
+// Input: strs = ["act","pots","tops","cat","stop","hat"]
+
+// Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+
+
+
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
