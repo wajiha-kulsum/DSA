aec0476a03cc5abee2b77bb9a2c29da6870026ae
Wajiha Kulsum <140605975+wajiha-kulsum@users.noreply.github.com>
2025-05-15 10:27:47 +0530

    updated

--- Top_K_Frequent_Elements.cpp
+++ Top_K_Frequent_Elements.cpp
@@ -3,7 +3,10 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> count;
         for (int num : nums)
-            
+        {
+            count[num]++;
+        }
+     
         vector<pair<int, int>> arr;
         for (const auto& p : count) {
             arr.push_back({p.second, p.first});
